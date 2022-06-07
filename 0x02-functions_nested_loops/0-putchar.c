0f7090c51e07d489dd2f4026c66d4c45a7bcb87c
Christian Koranteng <korantengchristian@gmail.com>
2022-06-07 12:24:25 -0700

    Correct 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- * This program  prints _putchar followed by a newline
+ * main - This program  prints _putchar followed by a newline
  *
- * main - entry point
- *
- * return: Always 0
+ * Return: 0
  *
  */
 
-
 int main(void)
-{       
-	char str = {"_","p","u","t","c","h","a","r"};
-	int c;
-	
-	for (c = 0;c < sizeof(str); c++)
-	{	
-	_putchar(c);
-        }  
-	_putchar("\n");
-	return (0);
+{
+	char putch = {"_", "p", "u", "t", "c", "h", "a", "r"};
 
+	int c;
+
+
+	for (c = 0; c < sizeof(putch); c++)
+	{
+
+		_putchar(c);
+
+	}
+
+	_putchar("\n");
+
+	return (0);
 }
 
