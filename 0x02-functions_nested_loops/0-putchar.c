aa3934491ab12b8a03e1fb65b24535c6c7cf6c6d
Christian Koranteng <korantengchristian@gmail.com>
2022-06-07 13:33:38 -0700

    Correct 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -17,7 +17,7 @@ int main(void)
 	for (c = 0; c < sizeof(putch); c++)
 	{
 
-		_putchar(putch[8]);
+		_putchar(putch[c]);
 
 	}
 
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point prints alphabets
+ *
+ * return: 0
+ *
+ *
+ */
+
+int main(void)
+{
+
+	char c;
+
+	while (c != 'z')
+	{
+		_putchar(c);
+		c++;
+	}	
+	return (0);
+}
