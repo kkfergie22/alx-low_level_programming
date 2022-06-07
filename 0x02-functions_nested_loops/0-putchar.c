3197537a60775eae029f360ed78b5b1d7ba59bdb
Christian Koranteng <korantengchristian@gmail.com>
2022-06-07 07:52:18 -0700

    Create )-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * This program  prints _putchar followed by a newline
+ *
+ * main - entry point
+ *
+ * return: Always 0
+ *
+ */
+
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
+
