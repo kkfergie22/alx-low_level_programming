b3defc6456747244538421adf9d210bbc6952a15
Christian Koranteng <korantengchristian@gmail.com>
2022-06-07 18:10:28 -0700

    Create Jack bauer

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  _islower - function to check case of character
+ *
+ *  c - is the variable evaluated
+ *
+ *  return: 0
+ */
+
+int _islower(int c)
+{
+	if (c == 'a' && c == 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
