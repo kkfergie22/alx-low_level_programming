c8fc0549fc23ad43a7c37d47e5ad32f47ef9117a
Christian Koranteng <korantengchristian@gmail.com>
2022-06-08 01:12:11 +0000

    Create Jack Bauer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints the last digit
+ * @c:  is the argument of the function
+ * Return: 0
+ */
+int print_last_digit(int c)
+{
+	if (c > 0 || c == 0)
+	{
+	_putchar (c % 10 + '0');
+	return (c % 10);
+	}
+	else
+	{
+	c = c * -1;
+	_putchar (c % 10 + '0');
+	return (c % 10);
+	}
+}
