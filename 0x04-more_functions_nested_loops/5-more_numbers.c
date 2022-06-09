8ab9669c818763705eded3b5e617b3b929dac774
Christian Koranteng <korantengchristian@gmail.com>
2022-06-09 22:30:43 +0000

    Create 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints the numbers from 0 to 9 10 times
+ * _putchar - prints characterse
+ *
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	char c;
+
+	while (i <= 0)
+	{
+		char c = '0';
+
+		while (c <= '14')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
