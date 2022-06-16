dc32bb3136eb42eb5d593c67d18ca11a94482c30
Christian Koranteng <korantengchristian@gmail.com>
2022-06-16 21:38:44 +0000

    Correct 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,6 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-
-	/*check to see if string is valid*/
-	if (dest == NULL && src == NULL)
-	{
-		return (NULL);
-	}
-
 	/*copy first n characters of src*/
 	while (i != n)
 	{
