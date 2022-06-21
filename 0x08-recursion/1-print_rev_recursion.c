d4f3d1bd8e1c17abbf297260efd62fcf6fee5edf
Christian Koranteng <korantengchristian@gmail.com>
2022-06-21 18:47:27 +0000

    Create task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the character string
+ * return: nothing
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i, length;
+
+	if (i < length / 2)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+		i++;
+		_print_rev_recursion(s);
+	}
+}
