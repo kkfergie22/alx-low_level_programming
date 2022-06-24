826219cbe7bbf9fc987571564f8b10861fd01b3f
Christian Koranteng <korantengchristian@gmail.com>
2022-06-24 21:33:47 +0000

    Update all

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char **argv);
+int checker(char *c);
 
 #endif
