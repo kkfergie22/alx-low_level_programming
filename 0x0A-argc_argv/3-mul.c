a4f71e751a9780f0eb419ac17fbcc922e28da569
Christian Koranteng <korantengchristian@gmail.com>
2022-06-24 14:14:04 +0000

    update task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,15 +12,23 @@
 
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]), b = atoi(argv[2]), product;
+	int a, b, product;
+
+	a = atoi(argv[1]); 
+	b = atoi(argv[2]);
 
 	/*Check if arguments are exactly two*/
-	if (argc < 2 || argc > 2)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	else
+	{
 
 	product = a * b;
+	printf("%d\n", product);
+	}
+
 	return (0);
 }
