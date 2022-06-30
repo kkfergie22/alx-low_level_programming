87235545bdb38ce9e843f69a55593a113e584528
Christian Koranteng <korantengchristian@gmail.com>
2022-06-30 17:37:54 +0000

    Update task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,8 +2,7 @@
 #include "main.h"
 /**
  * malloc_checked - checks if memory is allocated properly
- * check: the check variable
- *@b: the variable passed
+ *@b: the size of the memory
  *
  * Return: a pointer to the allocated memory
  *
@@ -13,7 +12,14 @@ void *malloc_checked(unsigned int b)
 {
 	unsigned int *check;
 
-	check = malloc(sizeof(unsigned int) * check);
+	check = malloc(sizeof(unsigned int) * b);
+
+	if (check == NULL)
+	{
+		return (98);
+	}
 
 	return (check);
+
+	free(check);
 }
