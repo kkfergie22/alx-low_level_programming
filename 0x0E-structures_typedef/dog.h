2678dde7919e480b681dc0e14057bc664f7efc07
Christian Koranteng <korantengchristian@gmail.com>
2022-07-04 21:24:45 +0000

    Complete work

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ char *owner;
 /**
  * dog - typedef for struct dog
  */
-typedef struct dog dog_d;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);dog_t *new_dog(char *name, float age, char *owner);
