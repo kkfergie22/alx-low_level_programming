07cb3478fb7e7b6815b3e7218c923de176500dc6
Christian Koranteng <korantengchristian@gmail.com>
2022-07-04 19:02:18 +0000

    Update task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-* struct dog - detais of a dog
+* struct dog - a struct of detais of a dog
 * @name: character pointer
 * @age: age of the dog
 * @owner: owner of the dog
  */
-typedef struct dog
+
+struct dog
 {
 char *name;
 float age;
 char *owner;
-}dog;
+};
+
+/**
+ * dog - typedef for struct dog
+ */
+typedef struct dog dog_d;
 
 #endif
