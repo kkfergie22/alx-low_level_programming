915d2a0ffba7fc92cf23e6ce84a8fa6007ae975b
Christian Koranteng <korantengchristian@gmail.com>
2022-07-04 18:48:39 +0000

    Create directory and first task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - detais of a dog
+* @name: character pointer
+* @age: age of the dog
+* @owner: owner of the dog
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
