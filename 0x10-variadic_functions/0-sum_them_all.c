a3653a7f2b25e50c5515109ffda7522bcaeb0770
Christian Koranteng <korantengchristian@gmail.com>
2022-07-07 21:22:20 +0000

    Update 0 to 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,15 +11,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-  va_list args;
-  int sum = 0;
-  unsigned int i;
+va_list args;
+int sum = 0;
+unsigned int i;
 
-  va_start(args, n);
-  for (i = 0; i < n; i++)
-  {
-    sum = sum + va_arg(args, int);
-  }
-  va_end(args);
-  return (sum);
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+sum = sum + va_arg(args, int);
+}
+va_end(args);
+return (sum);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,18 +14,18 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-  va_list args;
-  va_start(args, n);
-  unsigned int i;
+va_list args;
+va_start(args, n);
+unsigned int i;
 
-  for (i = 0; i < n; i++)
-  {
-    printf("%s", va_arg(args, char *));
-    if (i < n - 1)
-    {
-      printf("%s", separator);
-    }
-  }
-  va_end(args);
-  printf("\n");
+for (i = 0; i < n; i++)
+{
+printf("%s", va_arg(args, char *));
+if (i < n - 1)
+{
+printf("%s", separator);
+}
+}
+va_end(args);
+printf("\n");
 }
