f325c4be21df666f73b3abf595b020fd83cfa4e9
Christian Koranteng <korantengchristian@gmail.com>
2022-11-13 19:37:44 +0000

    Update 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,20 +5,17 @@
 def island_perimeter(grid):
     '''island_perimeter'''
 
-    cell_perimeter = 0
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
-            if grid[y][x] == 1:
-                try:
-                    if grid[y - 1][x] == 0:
-                        cell_perimeter = cell_perimeter + 1
-                    if grid[y][x - 1] == 0:
-                        cell_perimeter = cell_perimeter + 1
-                    if x == 0 or grid[y][x + 1] == 0:
-                        cell_perimeter = cell_perimeter + 1
-                    if y == 0 or grid[y + 1][x] == 0:
-                        cell_perimeter = cell_perimeter + 1
-                except IndexError:
-                    cell_perimeter = cell_perimeter + 1
-                    pass
-    return (cell_perimeter)
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
