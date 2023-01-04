c40a525a81ec1e76e34a1eda8d4a7588cae4d7d4
Christian Koranteng <korantengchristian@gmail.com>
2023-01-04 16:25:58 +0000

    Upload big O

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+ * @brief
+ *
+ * @param array
+ * @param size
+ * @param value
+ * @return int
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+}
